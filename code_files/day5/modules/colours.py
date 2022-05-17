790866ddcb097be5d30ccdfd5dc89913fcc750c9
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2022-05-17 09:19:17 +0530

    Add files via upload

---
+++ code_files/day5/modules/colours.py
@@ -0,0 +1,15 @@
+"amazing module, please use"
+
+def blue():
+    "use this function for blue"
+    print("cool blue")
+    
+def yellow():
+    print("bright yellow")
+    
+def green():
+    blue()
+    yellow()
+    
+def red():
+    print("scarlet red")
